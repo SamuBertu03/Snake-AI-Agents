22877f3e57a869b5c2ae4dce00129ea3d5653980
SamuBertu03 <145657992+SamuBertu03@users.noreply.github.com>
2025-11-19 15:15:58 +0100

    Update search_agents.py
    Merging original problem with relaxed problem

--- search_agents.py
+++ search_agents.py
@@ -1,8 +1,7 @@
 from collections import deque
 import heapq
-from math import sqrt
 from heuristics import manhattan
-# mosse
+
 MOVES = {
     "UP": (-1, 0),
     "DOWN": (1, 0),
@@ -11,8 +10,6 @@ MOVES = {
 }
 
 
-
-# def della classe dell'oggetto che ha come attributi il risultato della ricerca
 class SearchResult:
     def __init__(self, path, nodes_expanded, depth, cost, found):
         self.path = path
@@ -21,174 +18,168 @@ class SearchResult:
         self.cost = cost
         self.found = found
 
-# Agente base: restituisce le celle adiacenti alla posizione attuale 
+
 class _BaseAgent:
-    def __init__(self,is_relaxed=True):
-        self.is_relaxed=is_relaxed
-    
-    def _neighbors(self, game, pos):
-        # Caso rilassato in cui lo stato corrente contiene solo la testa del serpente
-        if(self.is_relaxed):
-            for dx, dy in MOVES.values():
-                nx, ny = pos[0] + dx, pos[1] + dy
-                if 0 <= nx < game.grid_size and 0 <= ny < game.grid_size:
-                    if (nx, ny) not in game.snake:
-                        yield (nx, ny)
-        else:
-            #applicazione caso completo
-            pass
-# Agente per BFS 
+    def _next_states(self, snake, food, grid_size):
+        head_x, head_y = snake[0]
+
+        for dx, dy in MOVES.values():
+            nx, ny = head_x + dx, head_y + dy
+
+            if not (0 <= nx < grid_size and 0 <= ny < grid_size):
+                continue
+
+            new_head = (nx, ny)
+
+            if new_head in snake:
+                continue
+
+            new_snake = [new_head] + list(snake)
+
+            if new_head == food:
+                new_food = None
+            else:
+                new_snake.pop()
+                new_food = food
+
+            yield new_head, new_snake, new_food
+
+
 class BFSAgent(_BaseAgent):
-    
-    def __init__(self, is_relaxed=True):
-        super().__init__(is_relaxed)
-        
     def find_path_with_exploration(self, game, on_expand=None):
         start, goal = game.snake[0], game.food
         if goal is None:
             return SearchResult([], 0, 0, 0, False)
 
-        queue = deque([(start, [start])])
-        visited = {start}
+        queue = deque([(game.snake, [], game.food)])
+        visited = {tuple(game.snake)}
         nodes_expanded = 0
 
         while queue:
-            pos, path = queue.popleft()
+            snake, path, food = queue.popleft()
+            head = snake[0]
             nodes_expanded += 1
 
-            # far vedere la nuova frontiera espansa durante il reasoning
             if on_expand:
-                on_expand(path, set(visited), nodes_expanded, len(queue))
+                visited_heads = {state[0] for state in visited}
+                on_expand(path, visited_heads, nodes_expanded, len(queue))
 
-            if pos == goal:
-                return SearchResult(path[1:], nodes_expanded, len(path), len(path) - 1, True)
+            if head == goal:
+                return SearchResult(path, nodes_expanded, len(path), len(path), True)
 
-            for nb in self._neighbors(game, pos):
-                if nb not in visited:
-                    visited.add(nb)
-                    queue.append((nb, path + [nb]))
+            for new_head, new_snake, new_food in self._next_states(snake, food, game.grid_size):
+                state_key = tuple(new_snake)
+                if state_key not in visited:
+                    visited.add(state_key)
+                    queue.append((new_snake, path + [new_head], new_food))
 
         return SearchResult([], nodes_expanded, 0, 0, False)
 
     def find_path(self, game):
         return self.find_path_with_exploration(game, on_expand=None)
 
-# Agente per DFS 
+
 class DFSAgent(_BaseAgent):
-    
-    def __init__(self, is_relaxed=True):
-        super().__init__(is_relaxed)
-        
     def find_path_with_exploration(self, game, on_expand=None):
         start, goal = game.snake[0], game.food
         if goal is None:
             return SearchResult([], 0, 0, 0, False)
 
-        stack = [(start, [start])]
-        visited = {start}
+        stack = [(game.snake, [], game.food)]
+        visited = {tuple(game.snake)}
         nodes_expanded = 0
 
         while stack:
-            pos, path = stack.pop()
+            snake, path, food = stack.pop()
+            head = snake[0]
             nodes_expanded += 1
 
             if on_expand:
-                on_expand(path, set(visited), nodes_expanded, len(stack))
+                visited_heads = {state[0] for state in visited}
+                on_expand(path, visited_heads, nodes_expanded, len(stack))
 
-            if pos == goal:
-                return SearchResult(path[1:], nodes_expanded, len(path), len(path) - 1, True)
+            if head == goal:
+                return SearchResult(path, nodes_expanded, len(path), len(path), True)
 
-            for nb in self._neighbors(game, pos):
-                if nb not in visited:
-                    visited.add(nb)
-                    stack.append((nb, path + [nb]))
+            for new_head, new_snake, new_food in self._next_states(snake, food, game.grid_size):
+                state_key = tuple(new_snake)
+                if state_key not in visited:
+                    visited.add(state_key)
+                    stack.append((new_snake, path + [new_head], new_food))
 
         return SearchResult([], nodes_expanded, 0, 0, False)
 
     def find_path(self, game):
         return self.find_path_with_exploration(game, on_expand=None)
 
-# Agente per Greedy 
+
 class GreedyAgent(_BaseAgent):
-    
-    def __init__(self, is_relaxed=True):
-        super().__init__(is_relaxed)
-        
     def find_path_with_exploration(self, game, on_expand=None):
         start, goal = game.snake[0], game.food
         if goal is None:
             return SearchResult([], 0, 0, 0, False)
 
-        # da mettere come param
-        open_list = [(manhattan(start, goal), [start])]
-        visited = {start}
+        open_list = [(manhattan(start, goal), game.snake, [], game.food)]
+        visited = {tuple(game.snake)}
         nodes_expanded = 0
 
         while open_list:
-            # scegli il path con h minore
-            open_list.sort(key=lambda item: manhattan(item[1][-1], goal))
-            _, path = open_list.pop(0)
-            pos = path[-1]
+            open_list.sort(key=lambda item: manhattan(item[1][0], goal))
+            _, snake, path, food = open_list.pop(0)
+            head = snake[0]
             nodes_expanded += 1
 
             if on_expand:
-                on_expand(path, set(visited), nodes_expanded, len(open_list))
+                visited_heads = {state[0] for state in visited}
+                on_expand(path, visited_heads, nodes_expanded, len(open_list))
 
-            if pos == goal:
-                return SearchResult(path[1:], nodes_expanded, len(path), len(path) - 1, True)
+            if head == goal:
+                return SearchResult(path, nodes_expanded, len(path), len(path), True)
 
-            for nb in self._neighbors(game, pos):
-                if nb not in visited:
-                    visited.add(nb)
-                    open_list.append((manhattan(nb, goal), path + [nb]))
+            for new_head, new_snake, new_food in self._next_states(snake, food, game.grid_size):
+                state_key = tuple(new_snake)
+                if state_key not in visited:
+                    visited.add(state_key)
+                    open_list.append((manhattan(new_head, goal), new_snake, path + [new_head], new_food))
 
         return SearchResult([], nodes_expanded, 0, 0, False)
 
     def find_path(self, game):
         return self.find_path_with_exploration(game, on_expand=None)
 
-# Agente per A star 
+
 class AStarAgent(_BaseAgent):
-    
-    def __init__(self, is_relaxed=True):
-        super().__init__(is_relaxed)
-        
     def find_path_with_exploration(self, game, on_expand=None):
         start, goal = game.snake[0], game.food
         if goal is None:
             return SearchResult([], 0, 0, 0, False)
 
-        # (priority, g, path)
-        open_list = [(manhattan(start, goal), 0, [start])]
-        visited = {start}
+        open_list = [(manhattan(start, goal), 0, game.snake, [], game.food)]
+        visited = {tuple(game.snake)}
         nodes_expanded = 0
 
         while open_list:
-            priority, g, path = heapq.heappop(open_list)
-            pos = path[-1]
+            f, g, snake, path, food = heapq.heappop(open_list)
+            head = snake[0]
             nodes_expanded += 1
 
             if on_expand:
-                on_expand(path, set(visited), nodes_expanded, len(open_list))
+                visited_heads = {state[0] for state in visited}
+                on_expand(path, visited_heads, nodes_expanded, len(open_list))
 
-            if pos == goal:
-                return SearchResult(path[1:], nodes_expanded, len(path), g, True)
+            if head == goal:
+                return SearchResult(path, nodes_expanded, len(path), g, True)
 
-            for nb in self._neighbors(game, pos):
-                if nb not in visited:
-                    visited.add(nb)
+            for new_head, new_snake, new_food in self._next_states(snake, food, game.grid_size):
+                state_key = tuple(new_snake)
+                if state_key not in visited:
+                    visited.add(state_key)
                     new_g = g + 1
-                    f = new_g + manhattan(nb, goal)
-                    heapq.heappush(open_list, (f, new_g, path + [nb]))
+                    f = new_g + manhattan(new_head, goal)
+                    heapq.heappush(open_list, (f, new_g, new_snake, path + [new_head], new_food))
 
         return SearchResult([], nodes_expanded, 0, 0, False)
 
     def find_path(self, game):
         return self.find_path_with_exploration(game, on_expand=None)
 
-
-    # TO DO
-    # Cicli hamiltoniani
-    # F(x) = h(x) + cicli hamiltoniani [ibrido]
-
-    
